c23c5d587856e00e9b7d0a13fbc830604523bfcd
Douglas <douglasacosta23@gmail.com>
2022-04-28 14:24:16 -0600

    protegiendo rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 export const AppRouter = () => {
@@ -10,8 +12,20 @@ export const AppRouter = () => {
 
         <Routes>
 
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/*" element={ <DashboardRoutes /> } />
+            <Route path="/login" element={ 
+              <PublicRoute>
+                <LoginScreen />
+              </PublicRoute> } />
+
+
+            {/* <Route path="/login" element={<LoginScreen />} /> */}
+
+            <Route path="/*" element={ 
+              <PrivateRoute>
+                <DashboardRoutes />
+              </PrivateRoute> } />
+
+            {/* <Route path="/*" element={ <DashboardRoutes /> } /> */}
 
         </Routes>       
     </BrowserRouter>
